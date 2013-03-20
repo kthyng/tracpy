d90751189d8010ca959931f0c6ee01ba253a3899
Kristen M. Thyng <kthyng@gmail.com>
2013-03-20 17:35:03 -0500

    wrapped up initial altering into a time loop on one computer, switching computers to debug

--- run.py
+++ run.py
@@ -53,13 +53,14 @@ nsteps = 10 # Number of steps to do between model outputs
 ndays = 5 # number of days to track the particles
 ff = 1 # forward
 # Start date
-date = datetime(year, month, day, hour)
+date = datetime(2009, 9, 1, 0)
 # Convert date to number
 date = netCDF.date2num(date,units)
 # Time between outputs
 Dt = 14400. # in seconds (4 hours), nc.variables['dt'][:] 
 # Number of model outputs to use
 tout = np.int((ndays*(24*3600))/Dt)
+tseas = 4*3600 # 4 hours between outputs 
 
 # Need to input x,y as relative to their grid box. Let's assume they are at 
 # some position relative to a grid node
@@ -106,10 +107,11 @@ pn = grid.variables['pn'][:] # 1/dy
 # Some grid metrics
 s = nc.variables['s_w'][:] # sigma coords, 31 layers
 cs = nc.variables['Cs_w'][:] # stretching curve in sigma coords, 31 layers
-# The following are for setting up z array on u grid for calculating u fluxes
-# Want h only within domain in y direction (no ghost cells) and interpolated onto cell
-# walls in x direction
-h = grid.variables['h'][1:-1,:]
+# The following are for setting up z array on u (v) grids for calculating u (v) fluxes
+# Want h only within domain in y (x) direction (no ghost cells) and interpolated onto cell
+# walls in x (y) direction
+hu = op.resize(grid.variables['h'][1:-1,:],1)
+hv = op.resize(grid.variables['h'][:,1:-1],0)
 
 files = sort(glob.glob('ocean_his_*.nc')) # sorted list of file names
 
@@ -178,87 +180,89 @@ else: #backwards in time
 		i = i + 1
 # model output files together containing all necessary model outputs
 nc = netCDF.MFDataset(fname) # reopen since needed to close things in loop
+dates = nc.variables['ocean_time'][:]	
+t0save = dates[0] # time at start of file in seconds since 1970-01-01, add this on at the end since it is big
 
+# Initialize drifter grid positions and indices
+xend = np.ones(((tinds-1)*nsteps,ia.size))*np.nan
+yend = np.ones(((tinds-1)*nsteps,ia.size))*np.nan
+zend = np.ones(((tinds-1)*nsteps,ia.size))*np.nan
+iend = np.ones(((tinds-1)*nsteps,ia.size))*np.nan
+jend = np.ones(((tinds-1)*nsteps,ia.size))*np.nan
+kend = np.ones(((tinds-1)*nsteps,ia.size))*np.nan
+t0 = np.zeros(((tinds-1)*nsteps+1))
+flag = np.zeros((ia.size),dtype=np.int) # initialize all exit flags for in the domain
+
+j = 0 # index for number of saved steps for drifters
 # Loop through model outputs
 for tind in tinds:
-	# Read in ROMS model output velocity fields at time index tind
+	# Read in ROMS model output velocity fields at time index tind one index
+	# in front or behind depending on time direction
 	# Only read in actual cells not ghost cells. Have to use only actual cells
 	# in order to have all fully-formed cells (with all walls)
-	u = nc.variables['u'][tind,:,1:-1,:] 
-	v = nc.variables['v'][tind,:,:,1:-1]
-	h = op.resize(grid.variables['h'][1:-1,:],1)
+	if ff: # forward
+		u = nc.variables['u'][tind:tind+2,:,1:-1,:] 
+		v = nc.variables['v'][tind:tind+2,:,:,1:-1]
+		zetau = op.resize(nc.variables['zeta'][tind:tind+2,1:-1,:],2) # [t,j,i]
+		zetav = op.resize(nc.variables['zeta'][tind:tind+2,:,1:-1],2) # [t,j,i]
+	else:
+		u = nc.variables['u'][tind-1:tind-3,:,1:-1,:] 
+		v = nc.variables['v'][tind-1:tind-3,:,:,1:-1]
+		zetau = op.resize(nc.variables['zeta'][tind-1:tind-3,1:-1,:],2) # [t,j,i]
+		zetav = op.resize(nc.variables['zeta'][tind-1:tind-3,:,1:-1],2) # [t,j,i]
 
-	zeta = op.resize(nc.variables['zeta'][tind,1:-1,:],2) # [t,k,j,i]
-	pdb.set_trace()
 
 	# WHY DO I HAVE TO CALCULATE THE FLUXES HERE WHEN THE BOX VOLUMES ARE ALSO CALCULATED IN THE CODE?
-	# Get zeta. Same as with h (but there is a time dimension first)
-	# Create 3D grid information arrays	
-	# 4D zeta for creating full 3D grid in time # [t,k,j,i]
-	zeta4 = zeta.reshape(zeta.shape[0],1,zeta.shape[1],zeta.shape[2]) \
+	# Creating full vertical grid in time # [t,k,j,i]
+	zeta4u = zetau.reshape(zetau.shape[0],1,zetau.shape[1],zetau.shape[2]) \
 						.repeat(len(cs),axis=1)
-	cs4 = cs.reshape(1,len(cs),1,1) \
-						.repeat(zeta.shape[0],axis=0) \
-						.repeat(zeta.shape[1],axis=2) \
-						.repeat(zeta.shape[2],axis=3)
-	h4 = h.reshape(1,1,h.shape[0],h.shape[1]) \
-						.repeat(zeta.shape[0],axis=0) \
+	cs4u = cs.reshape(1,len(cs),1,1) \
+						.repeat(zetau.shape[0],axis=0) \
+						.repeat(zetau.shape[1],axis=2) \
+						.repeat(zetau.shape[2],axis=3)
+	h4u = hu.reshape(1,1,hu.shape[0],hu.shape[1]) \
+						.repeat(zetau.shape[0],axis=0) \
 						.repeat(len(cs),axis=1)	
-	z4 = zeta4 + cs4*(zeta4+h4)
-	del(zeta4,cs4,h4)
-	dz4 = np.diff(z4,axis=1).reshape((u.shape)) # grid cell thickness in z coords
-	###
-	# Change velocity fields to fluxes. DO TWO TIME INDICES AT ONCE
+	z4u = zeta4u + cs4u*(zeta4u+h4u)
+	del(zeta4u,cs4u,h4u)
+	dz4u = np.diff(z4u,axis=1).reshape((u.shape)) # grid cell thickness in z coords
+	# Change velocity fields to fluxes.
 	# Calculate uflux, dependent on time. Calculating for two time indices at once.
 	# Use pn for dy
 	dyu = 1/op.resize(pn[1:-1,:],1) # [JMT-1,IMT]
 	# uflux is size [2,KM,JMT-1,IMT]
-	uflux = u*dyu*dz4
-	# pdb.set_trace()
+	uflux = u*dyu*dz4u
 
 	# Do for vfluxes
-	### Set up z array on u grid for calculating v fluxes
-	# Want h only within domain in x direction (no ghost cells) and interpolated onto cell
-	# walls in y direction
-	# should be able to clean these up with array broadcasting
-	h = op.resize(grid.variables['h'][:,1:-1],0)
-	# Get zeta. Same as with h (but there is a time dimension first)
-	zeta = op.resize(nc.variables['zeta'][it:it+2,:,1:-1],1) # [t,k,j,i]
-	# Create 3D grid information arrays	
-	# 4D zeta for creating full 3D grid in time # [t,k,j,i]
-	zeta4 = zeta.reshape(zeta.shape[0],1,zeta.shape[1],zeta.shape[2]) \
+	zeta4v = zetav.reshape(zetav.shape[0],1,zetav.shape[1],zetav.shape[2]) \
 						.repeat(len(cs),axis=1)
-	cs4 = cs.reshape(1,len(cs),1,1) \
-						.repeat(zeta.shape[0],axis=0) \
-						.repeat(zeta.shape[1],axis=2) \
-						.repeat(zeta.shape[2],axis=3)
-	h4 = h.reshape(1,1,h.shape[0],h.shape[1]) \
-						.repeat(zeta.shape[0],axis=0) \
+	cs4v = cs.reshape(1,len(cs),1,1) \
+						.repeat(zetav.shape[0],axis=0) \
+						.repeat(zetav.shape[1],axis=2) \
+						.repeat(zetav.shape[2],axis=3)
+	h4v = hv.reshape(1,1,hv.shape[0],hv.shape[1]) \
+						.repeat(zetav.shape[0],axis=0) \
 						.repeat(len(cs),axis=1)	
-	z4 = zeta4 + cs4*(zeta4+h4)
-	del(zeta4,cs4,h4)
-	dz4 = np.diff(z4,axis=1).reshape((v.shape)) # grid cell thickness in z coords
-	###
-	# Calculate uflux, dependent on time. Calculating for two time indices at once.
-	# Use pn for dy
-	dxv = 1/op.resize(pn[:,1:-1],0) # [JMT,IMT-1]
+	z4v = zeta4v + cs4v*(zeta4v+h4v)
+	del(zeta4v,cs4v,h4v)
+	dz4v = np.diff(z4v,axis=1).reshape((v.shape)) # grid cell thickness in z coords
+	# Calculate vflux, dependent on time. Calculating for two time indices at once.
+	# Use pm for dx
+	dxv = 1/op.resize(pm[:,1:-1],0) # [JMT,IMT-1]
 	# vflux is size [2,KM,JMT,IMT-1]
-	vflux = v*dxv*dz4
-
+	vflux = v*dxv*dz4v
 
 	# dxdy is the horizontal area of the cell walls, so it should be the size of the number of cells only
 	# I am calculating it by adding the area of the walls together for each cell
 	dxdy = ((dyu[:,1:]+dyu[:,0:-1])+(dxv[1:,:]+dxv[0:-1,:])) # [JMT-1,IMT-1]
 
 	# calculate dxyz here
-	dxyz = op.resize(dz4,2)*dxdy
+	dxyz = op.resize(dz4v,2)*dxdy
 
 	nc.close()
 	grid.close()
 
-	t0save = t[0] # time at start of file in seconds since 1970-01-01, add this on at the end since it is big
-	tseas = 4*3600 # 4 hours between outputs 
-	hs = op.resize(zeta,1) # sea surface height in meters
+	# hs = op.resize(zeta,1) # sea surface height in meters
 	# dz = np.diff(z4,axis=1)[0,:,0,0] # just take one dz column for now
 
 	# make arrays in same order as is expected in the fortran code
@@ -267,14 +271,14 @@ for tind in tinds:
 	vflux = vflux.T
 	# dxdy = dxdy.T
 	dxyz = dxyz.T
-	hs = hs.T
+	# hs = hs.T
 
 	# change all arrays to be fortran-directioned instead of python
 	uflux = uflux.copy(order='f') # [2,KM,JMT-1,IMT] (all of these are swapped now due to transposes above)
 	vflux = vflux.copy(order='f') #[2,KM,JMT,IMT-1]]
 	# dxdy = dxdy.copy(order='f') #[JMT,IMT]]
 	dxyz = dxyz.copy(order='f') #[2,KM,JMT-1,IMT-1]
-	hs = hs.copy(order='f') # [JMT-1,IMT-1]
+	# hs = hs.copy(order='f') # [JMT-1,IMT-1]
 	# These are the size of the psi grid, at the corners of the grid boxes.
 	# This might need to change to the number of boxes in the future, which will
 	# change all of the minus one's. Also would change the default values in the step call.
@@ -282,39 +286,24 @@ for tind in tinds:
 	JMT=v.shape[2] # 190
 	KM=u.shape[1] # 30
 
-	# Loop over model outputs
-	xend = np.ones((nsteps,ia.size))*np.nan
-	yend = np.ones((nsteps,ia.size))*np.nan
-	zend = np.ones((nsteps,ia.size))*np.nan
-	iend = np.ones((nsteps,ia.size))*np.nan
-	jend = np.ones((nsteps,ia.size))*np.nan
-	kend = np.ones((nsteps,ia.size))*np.nan
 	ufluxinterp = uflux.copy()
 	vfluxinterp = vflux.copy()
 	# hsinterp = hs.copy()
 	dxyzinterp = dxyz.copy()
-	t0 = np.zeros((nsteps+1))
-	flag = np.zeros((ia.size),dtype=np.int) # initialize all exit flags for in the domain
-	# Loop over iterations between two model outputs
+	# Loop over nsteps iterations between two model outputs
 	for i in xrange(nsteps):
 		#  flux fields at starting time for this step
-		if i != 0:
+		if j != 0:
 			ufluxinterp[:,:,:,0] = ufluxinterp[:,:,:,1]
 			vfluxinterp[:,:,:,0] = vfluxinterp[:,:,:,1]
 			# hsinterp[:,:,0] = hsinterp[:,:,1]
 			dxyzinterp[:,:,:,0] = dxyzinterp[:,:,:,1]
-			xstart = xend[i-1,:]
-			ystart = yend[i-1,:]
-			zstart = zend[i-1,:]
-			ia = iend[i-1,:]
-			ja = jend[i-1,:]
-			ka = kend[i-1,:]
-			# ia = np.ceil(xend[i-1,:])
-			# ja = np.ceil(yend[i-1,:])
-			# ka = np.ceil(zend[i-1,:])
-			# ia = xend[i-1,:].astype(int)
-			# ja = yend[i-1,:].astype(int)
-			# ka = zend[i-1,:].astype(int)
+			xstart = xend[j-1,:]
+			ystart = yend[j-1,:]
+			zstart = zend[j-1,:]
+			ia = iend[j-1,:]
+			ja = jend[j-1,:]
+			ka = kend[j-1,:]
 			# mask out drifters that have exited the domain
 			xstart = np.ma.masked_where(flag[:]==1,xstart)
 			ystart = np.ma.masked_where(flag[:]==1,ystart)
@@ -341,16 +330,17 @@ for tind in tinds:
 		if not np.ma.compressed(xstart).any(): # exit if all of the drifters have exited the domain
 			break
 		else:
-			xend[i,ind],yend[i,ind],zend[i,ind],iend[i,ind],jend[i,ind],kend[i,ind],flag[ind] = \
+			xend[j,ind],yend[j,ind],zend[j,ind],iend[j,ind],jend[j,ind],kend[j,ind],flag[ind] = \
 														tracmass.step(np.ma.compressed(xstart),np.ma.compressed(ystart),
 														np.ma.compressed(zstart),0.,np.ma.compressed(ia),np.ma.compressed(ja),
 														np.ma.compressed(ka),tseas/float(nsteps),ufluxinterp,
 														vfluxinterp,ff,dxyzinterp)#dz.data,dxdy)
 			# if np.sum(flag)>0:
 			# pdb.set_trace()
-			t0[i+1] = t0[i] + tseas/float(nsteps) # update time in seconds to match drifters
+			t0[j+1] = t0[j] + tseas/float(nsteps) # update time in seconds to match drifters
 			# if i == 8:
 			# 	pdb.set_trace()
+		j = j + 1
 
 t0 = t0 + t0save # add back in base time in seconds
 
