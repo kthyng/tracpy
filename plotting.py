dece6d0d1d2e347155f22afeecad43d012cdd03b
Kristen M. Thyng <kthyng@gmail.com>
2013-08-05 15:48:57 -0500

    tweaking transport plot

--- plotting.py
+++ plotting.py
@@ -343,7 +343,7 @@ def transport(name, fmod=None, Title=None, dmax=None, N=7, extraname=None,
     plot(x0, y0, 'go', markersize=10)
 
     # Inlaid colorbar
-    cax = fig.add_axes([0.48, 0.25, 0.41, 0.02])
+    cax = fig.add_axes([0.49, 0.25, 0.4, 0.02])
     # cax = fig.add_axes([0.5, 0.2, 0.35, 0.02])
     cb = colorbar(cax=cax,orientation='horizontal')
     cb.set_label('Normalized drifter transport (%)')
