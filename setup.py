3ba327edf1efcdbadab4a670d03aff0a7f1645eb
chris.barker <chris.barker@noaa.gov>
2013-11-06 15:42:21 -0800

    restructured as a proper package

--- setup.py
+++ setup.py
@@ -5,30 +5,41 @@ setup.py for tracpy -- python wrappers around TRACMASS
 
 """
 import shutil
-from distutils.core import Extension
 from setuptools import setup # to support "develop" mode
+from numpy.distutils.core import setup, Extension
 
-## total kludge:
-## this copies extension built with makefile to package
-## better to have setup.py build TRACMASS
+# ## total kludge:
+# ## this copies extension built with makefile to package
+# ## better to have setup.py build TRACMASS
 
-try:
-    shutil.copyfile("src/tracmass.so", "tracpy/tracmass.so")
+# try:
+#     shutil.copyfile("src/tracmass.so", "tracpy/tracmass.so")
 
-except IOError:
-    raise Exception("TRAMASS extesnion not built -- built it with the makefile in src")
+# except IOError:
+#     raise Exception("TRAMASS extesnion not built -- built it with the makefile in src")
 
-tracmass = Extension("tracpy/tracmass",
-                     sources=["src/tracmass.so"],
-                     )
+tracmass_mod = Extension(name = "tracmass",
+                         sources=['src/calc_dxyz.f95',
+                                  'src/cross.f95',
+                                  'src/loop_pos.f95',
+                                  'src/step.f95',
+                                  'src/vertvel.f95',
+                                  'src/calc_time.f95',
+                                  'src/diffusion.f95',
+                                  'src/pos.f95',
+                                  'src/turb.f95',
+                                  ],
+                      #extra_f90_compile_args=["-ffixed-form"],
+                      )
 
-modules = ["tracpy/inout.py",
-           "tracpy/op.py"
-           "tracpy/plotting.py",
-           "tracpy/run.py",
-           "tracpy/tools.py",
-           "tracpy/tracmass.so"
-           ]
+# modules = ["tracpy/inout.py",
+#            "tracpy/op.py"
+#            "tracpy/plotting.py",
+#            "tracpy/run.py",
+#            "tracpy/tools.py",
+#            ]
+
+print tracmass_mod
 
 setup(
     name = "tracpy",
@@ -42,7 +53,8 @@ setup(
     #             "Topic :: Utilities",
                  ],
     packages = ["tracpy"],
-    modules = modules, 
-    ext_modules = [tracmass],
+    # py_modules = modules,
+    ext_package='tracpy', 
+    ext_modules = [tracmass_mod],
     scripts = [],
     )
