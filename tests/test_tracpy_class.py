4d568e008bc18177393cf50e9a3d676009b01c89
Kristen M. Thyng <kthyng@gmail.com>
2014-01-02 15:48:17 -0600

    added files that are the start of a tracpy class and tests for that class

---
+++ tests/test_tracpy_class.py
@@ -0,0 +1,42 @@
+'''
+Testing TracPy class
+Call with py.test test_tracpy_class.py
+'''
+
+import tracpy
+from tracpy.tracpy_class import Tracpy
+import os
+
+# For niceties with file locations and such
+here = os.path.dirname(__file__)
+
+def test_init():
+	'''
+	Make sure that we can initialize the class.
+	'''
+	
+	tp = Tracpy(os.path.join(here, 'input', 'ocean_his_0001.nc'))
+
+	assert True
+
+def test_initWithGridSource():
+	'''
+	Make sure we can initialize with a grid source location
+	'''
+	
+	tp = Tracpy(os.path.join(here, 'input', 'ocean_his_0001.nc'), grid_source=os.path.join(here, 'input', 'grid.nc'))
+
+	assert True
+
+def test_readgrid():
+	'''
+	Test for initializing grid.
+	'''
+
+	tp = Tracpy(os.path.join(here, 'input', 'ocean_his_0001.nc'),
+				os.path.join(here, 'input', 'grid.nc'))
+	tp._readgrid()
+
+	assert True
+
+	assert tp.grid
--- tracpy/tracpy_class.py
+++ tracpy/tracpy_class.py
@@ -2,9 +2,60 @@
 
 '''
 TracPy class
+HAVE THINGS THAT ARE THE SAME FOR EACH STEP IN GNOME BE STORED IN 
+THIS CLASS
+* flush this out
+* (keep adding tests)
+* see where to put this into tracpy_mover.py
 '''
 
-class Tracpy(object):
-    pass
+import tracpy
 
-    
+class Tracpy(object):
+    '''
+    TracPy class.
+    '''
+
+    def __init__(self, loc_source, grid_source=None):
+        '''
+        Initialize class.
+
+        :param loc_source: NetCDF file name (with extension) or OpenDAP url.
+        :param grid_source=None: NetCDF grid file name or OpenDAP url.
+        '''
+
+        self.loc_source = loc_source
+        self.grid_source = grid_source
+        self.grid = None
+
+    def _readgrid(self):
+        '''
+        Read in horizontal and vertical grid.
+        '''
+
+        # BREAK UP READGRID INTO SMALLER FUNCTIONS LATER
+        # LOOK AT TIMING AGAIN (why is grid so slow) LATER
+
+        # if vertical grid information is not included in the grid file, or if all grid info
+        # is not in output file, use two
+        if self.grid_source is not None:
+            self.grid = tracpy.inout.readgrid(self.grid_source, vert_source=self.loc_source)
+        else:
+            self.grid = tracpy.inout.readgrid(self.loc_source)
+
+    def step():
+        '''
+        Take some number of steps between a start and end time.
+        '''
+
+        if self.grid is None:
+            self._readgrid()
+
+        # Figure out where in time we are 
+
+        # Load fields if not already loaded
+
+        # Interpolate the velocity fields
+
+        # Call TRACMASS
+
